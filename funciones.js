d0a4b956b9511c014187f8494458fff4924dfb47
NiicolasLencina98 <lenciina.niicolas@gmail.com>
2024-04-21 14:56:51 -0300

    Funciones

---
+++ funciones.js
@@ -0,0 +1,20 @@
+//Sintaxis y desarollo de una funcion
+function getReminder(){
+    console.log('Water the plants.')
+  }
+  
+  function greetInSpanish(){
+    console.log('Buenas tardes.')
+  }
+
+  //Llamar funciones
+ //creamos una funcion y la llamamos varias veces
+  function sayThanks(){
+    console.log('Thank you for your purchase! We appreciate your business.')
+  }
+  
+  sayThanks()
+  sayThanks()
+  sayThanks()
+  
+  //Parametros y argumentos en una funcion
